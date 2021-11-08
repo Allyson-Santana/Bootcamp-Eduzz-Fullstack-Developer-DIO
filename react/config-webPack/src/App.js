20bb320fad325d46e8e54979bb2d9ff07e6372e8
Allyson Santana <santana.allyson459@gmail.com>
2021-11-08 11:04:36 -0300

    Introdução a react

---
+++ react/config-webPack/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Card from './Card';
+
+function App()
+{
+    return(
+        <div className="main">
+            Hello World
+        </div>
+    );
+}
--- react/config-webPack/webpack.config.js
+++ react/config-webPack/webpack.config.js
@@ -1,9 +1,28 @@
 const path = require('path');
 
 module.exports = {
+    //mode: 'production',
+    sourcemap: true,
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundler.js'
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            flename: "./index.html"
+        })
+    ]
 }
